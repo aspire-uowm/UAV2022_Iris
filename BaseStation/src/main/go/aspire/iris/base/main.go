eca9ad001f4f6d5b70c1e31d63e59533e2005fe6
Mark Delaportas <delaportas.markos@gmail.com>
2023-11-16 01:31:38 +0000

    now we can start somewhere

--- BaseStation/src/main/go/aspire/iris/base/main.go
+++ BaseStation/src/main/go/aspire/iris/base/main.go
@@ -1,33 +1,15 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
-var db = make(map[string]string)
-
-func setupRouter() *gin.Engine {
-	// Disable Console Color
-	// gin.DisableConsoleColor()
-	router := gin.Default()
-	router.LoadHTMLGlob("templates/*")
-
-	router.GET("/index", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl", gin.H{
-			"title": "Main website",
+func main() {
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
 		})
 	})
-
-	return router
-}
-
-func main() {
-	r := setupRouter()
-
-	
-	
-	// Listen and Server in 0.0.0.0:8080
-	r.Run(":8080")
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
