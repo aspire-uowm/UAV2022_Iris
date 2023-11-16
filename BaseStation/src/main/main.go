d051122703dc4ecc386b055d87d53eb2bc9c794e
Mark Delaportas <delaportas.markos@gmail.com>
2023-11-16 13:38:20 +0000

    And we render html gents

---
+++ BaseStation/src/main/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.LoadHTMLGlob("./resources/templates/index.tmpl")
+
+	//router.LoadHTMLFiles("templates/template1.html")
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+	router.Run(":8080")
+}
