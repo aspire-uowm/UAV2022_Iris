fbec8f61406a4b05fb6483fd518c044e00acb51d
Mark Delaportas <delaportas.markos@gmail.com>
2023-11-16 02:16:49 +0200

    rearanged some things

---
+++ BaseStation/src/main/go/aspire/iris/base/main.go
@@ -0,0 +1,34 @@
+package aspire.iris.base
+
+import (
+	"net/http"
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	router := gin.Default()
+	router.LoadHTMLGlob("templates/*")
+
+	router.GET("/index", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Main website",
+		})
+	})
+
+	return router
+}
+
+func main() {
+	r := setupRouter()
+
+	
+	
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
